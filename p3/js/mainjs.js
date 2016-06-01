a086dbce0e821a03394f642d77ec95ac452164f8
Eli Bermudez <bermudez.86@live.com.mx>
2016-05-31 20:25:17 -0500

    Terminando la tercera

--- p3/js/mainjs.js
+++ p3/js/mainjs.js
@@ -1,9 +1,37 @@
-var animales = new Array("Caballo", "Gato", "Perro");
+function telefono(numero, marca, color) {
+ 
+    //Propiedades privadas
+    var contadorLlamadas=0;
 
+    //Propiedades públicas
+    this._numero = numero;
+    this._marca = marca;
+    this._color = color;
 
-function copiaArreglo(arregloOrigen){
-	var arregloCopia=arregloOrigen
-	return arregloCopia
+    //Métodos privados
+
+    //Métodos públicos
+    this.marcar = function(numeroMarcar) {
+        contadorLlamadas++;
+        return numeroMarcar;
+    };
+
+    this.contadordeLlamadas = function() {
+        return contadorLlamadas;
+    };
 }
 
-document.getElementById("respuesta").innerHTML = copiaArreglo(animales);
+var mytelefono = new telefono("00000","StarLight","Negro");
+
+function marcar(){
+    var numeroTelefono =document.getElementById("numeroTelefono").value;
+    document.getElementById("respuesta").innerHTML = "A marcado al siguiente numero: "+mytelefono.marcar(numeroTelefono);    
+}
+
+function verdatos(){
+    document.getElementById("respuesta").innerHTML = "Numero:"+mytelefono._numero+ " Marca:" +mytelefono._marca+ " Color:" +mytelefono._color;
+}
+
+function llamadasRealizadas(){
+    document.getElementById("respuesta").innerHTML = "Llamadas Realizadas: " + mytelefono.contadordeLlamadas();
+}

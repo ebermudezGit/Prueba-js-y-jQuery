0bf764f6ac2e764c9e33cbeb7d4bb832ee1c4092
Eli Bermudez <bermudez.86@live.com.mx>
2016-05-31 19:53:40 -0500

    Iniciando la tercera

---
+++ p3/js/mainjs.js
@@ -0,0 +1,9 @@
+var animales = new Array("Caballo", "Gato", "Perro");
+
+
+function copiaArreglo(arregloOrigen){
+	var arregloCopia=arregloOrigen
+	return arregloCopia
+}
+
+document.getElementById("respuesta").innerHTML = copiaArreglo(animales);

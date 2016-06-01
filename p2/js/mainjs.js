93b1ce03f36f7d3d09e79531c1d91cdae8eeffce
Eli Bermudez <bermudez.86@live.com.mx>
2016-05-31 19:44:40 -0500

    salbando pregunta dos

--- p2/js/mainjs.js
+++ p2/js/mainjs.js
@@ -1,9 +1,1 @@
-var suma = 0;
-do{
- var ingreso = prompt("ingresa un número");
- if(!isNaN(ingreso)){
- 	suma=suma+parseInt(ingreso);
- }
-}while (ingreso != "fin");
-console.log(suma);
-document.write("La respuesta es: "+suma);
+var animales = new Array("Caballo", "Gato", "Perro");

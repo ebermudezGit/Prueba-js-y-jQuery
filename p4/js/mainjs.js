ea4309070eb6a577dc242683d27f28f765e9a70c
Eli Bermudez <bermudez.86@live.com.mx>
2016-05-31 21:04:37 -0500

    Termina la cuarta Pregunta

---
+++ p4/js/mainjs.js
@@ -0,0 +1,20 @@
+
+var largo = prompt("Largo de la figura");
+
+var contarY=0;
+while (contarY<largo) {
+    var imprimir=""
+    var contarX=0;
+    while (contarX<largo) {
+        if(contarY==0 || contarY==(largo-1) || contarX==0 || contarX==(largo-1))
+            imprimir=imprimir+"*";
+        else
+            imprimir=imprimir+" ";
+        
+        contarX++;
+    }
+    //console.log(imprimir+"\r\n");
+
+    console.log(contarY+1,imprimir);
+    contarY++;
+}
